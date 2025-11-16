e9e988e2d63f2dbc83d9d0600a1ba48a872f3d1f
Gabriel Ford <gabe@fordltc.net>
2025-11-16 18:36:30 -0500

    start laying out main loop

--- src/main.c
+++ src/main.c
@@ -1,1 +1,33 @@
-int main() {return 0;}
+#include <stdbool.h>
+#include <stdint.h>
+
+struct inst {
+  enum tag {
+    CLEAR,
+    JUMP,
+  } tag;
+};
+
+struct state {
+  bool running;
+
+  uint16_t stack[16];
+  uint8_t heap[4096];
+};
+
+void init_state(struct state *state);
+int main_loop(struct state *state);
+
+int main() {
+  struct state state;
+  init_state(&state);
+  main_loop(&state);
+}
+
+void init_state(struct state *state) { state->running = true; }
+
+uint16_t fetch(uint16_t pc);
+uint16_t decode(uint16_t inst);
+uint16_t execute(uint16_t pc);
+
+int main_loop(struct state *state) {}
