a59459d40fabace0fb4285e08c4bd51fc677b55e
Gabriel Ford <gabe@fordltc.net>
2025-11-17 00:54:03 -0500

    setup display to draw 64x32 grid

--- src/main.c
+++ src/main.c
@@ -47,9 +47,9 @@ int main() {
 void init_state(struct state *state) {
   state->running = true;
   memset(state->display, 0, sizeof(state->display));
-  state->display[2][2] = 1;
 }
 
+void draw_grid(uint8_t display[DISPLAY_WIDTH][DISPLAY_HEIGHT]);
 uint16_t *fetch(uint8_t heap[4096], uint16_t pc);
 struct inst decode(uint16_t inst);
 // TODO: This isn't right
@@ -58,18 +58,22 @@ uint16_t execute(uint16_t pc);
 int main_loop(struct state *state) {
   while (!WindowShouldClose()) {
     // uint16_t *inst = fetch(state->heap, state->pc);
-    BeginDrawing();
-    ClearBackground(BLACK);
-    for (int y = 0; y < DISPLAY_HEIGHT; y++) {
-      for (int x = 0; x < DISPLAY_WIDTH; x++) {
-        if (state->display[x][y])
-          DrawRectangle(x * PIXEL_WIDTH, y * PIXEL_HEIGHT, PIXEL_WIDTH,
-                        PIXEL_HEIGHT, RAYWHITE);
-      }
-    }
-    EndDrawing();
+    draw_grid(state->display);
   }
   return 0;
+}
+
+void draw_grid(uint8_t display[DISPLAY_WIDTH][DISPLAY_HEIGHT]) {
+  BeginDrawing();
+  ClearBackground(BLACK);
+  for (int y = 0; y < DISPLAY_HEIGHT; y++) {
+    for (int x = 0; x < DISPLAY_WIDTH; x++) {
+      if (display[x][y])
+        DrawRectangle(x * PIXEL_WIDTH, y * PIXEL_HEIGHT, PIXEL_WIDTH,
+                      PIXEL_HEIGHT, RAYWHITE);
+    }
+  }
+  EndDrawing();
 }
 
 uint16_t *fetch(uint8_t heap[4096], uint16_t pc) {
