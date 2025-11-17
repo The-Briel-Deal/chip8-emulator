cb29646093e51d40b9c7df3266180785578a15ea
Gabriel Ford <gabe@fordltc.net>
2025-11-17 11:37:48 -0500

    use a uint64 for each row

--- src/main.c
+++ src/main.c
@@ -13,6 +13,8 @@
 #define PIXEL_WIDTH WINDOW_WIDTH / DISPLAY_WIDTH
 #define PIXEL_HEIGHT WINDOW_HEIGHT / DISPLAY_HEIGHT
 
+typedef uint64_t display[DISPLAY_HEIGHT];
+
 struct inst {
   enum tag {
     CLEAR,
@@ -27,7 +29,7 @@ struct state {
   bool running;
 
   // TODO: This is a single color for each pixel so we don't need 8 bits
-  uint8_t display[DISPLAY_WIDTH][DISPLAY_HEIGHT];
+  display display;
   uint16_t pc;
   uint16_t stack[16];
   uint8_t heap[4096];
@@ -41,6 +43,7 @@ int main() {
              "raylib [core] example - basic window");
   struct state state;
   init_state(&state);
+  state.display[3] = 32;
   main_loop(&state);
 }
 
@@ -49,7 +52,7 @@ void init_state(struct state *state) {
   memset(state->display, 0, sizeof(state->display));
 }
 
-void draw_grid(uint8_t display[DISPLAY_WIDTH][DISPLAY_HEIGHT]);
+void draw_grid(display display);
 uint16_t *fetch(uint8_t heap[4096], uint16_t pc);
 struct inst decode(uint16_t inst);
 // TODO: This isn't right
@@ -63,12 +66,13 @@ int main_loop(struct state *state) {
   return 0;
 }
 
-void draw_grid(uint8_t display[DISPLAY_WIDTH][DISPLAY_HEIGHT]) {
+void draw_grid(display display) {
   BeginDrawing();
   ClearBackground(BLACK);
   for (int y = 0; y < DISPLAY_HEIGHT; y++) {
     for (int x = 0; x < DISPLAY_WIDTH; x++) {
-      if (display[x][y])
+      uint64_t row = display[y];
+      if (row & ((uint64_t)(1) << x))
         DrawRectangle(x * PIXEL_WIDTH, y * PIXEL_HEIGHT, PIXEL_WIDTH,
                       PIXEL_HEIGHT, RAYWHITE);
     }
